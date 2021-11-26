eb6087daa62cc2904a31fae4f0a83b66a5c43fe4
Ubuntu <ubuntu@ip-172-31-92-216.ec2.internal>
2021-11-26 17:49:10 +0000

    Added Guess.java

---
+++ Guess.java
@@ -0,0 +1,100 @@
+/*
+* The "Guess" program makes you guess at a random number
+* until you get it right
+*
+* @author  Myles Trump
+* @version 1.0
+* @since   2020-11-24
+*/
+
+import java.util.Scanner;
+import java.util.Random;
+
+/**
+* This is the "Guess"  program.
+*/
+final class Guess {
+
+    /**
+    * Loop number.
+    */
+    public static final double LOOP_COUNTER = 1.0;
+
+    /**
+    * Prevent instantiation.
+    * Throw an exception IllegalStateException.
+    * if this ever is called
+    *
+    * @throws IllegalStateException
+    *
+    */
+    private Guess() {
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+
+    /**
+    * The starting main() function.
+    *
+    * @param args No args will be used
+    */
+
+    public static void main(final String[] args) {
+        // variables
+        double totalTries = 0;
+
+        final Scanner input = new Scanner(System.in);
+
+        // block of code to try
+        try {
+            // keeps track of how many attempts the user has done
+            totalTries = totalTries + 1;
+
+            // infinite loop until the break statement when correct
+            while (LOOP_COUNTER > 0) {
+
+                // instance of random class
+                int max = 6;
+                int min = 1;
+
+                int randNum = (int)Math.floor(Math.random()*(max-min+1)+min);
+                // generate random value from 1-6
+
+                System.out.print("Enter an integer between 1-6: ");
+
+                // stores user input
+                int userInput = input.nextInt();
+
+                // if the user gets it right
+                if (userInput == randNum) {
+                    System.out.print("\nYou have entered the correct answer! It took you "
+                        + totalTries + " tries!");
+                    break;
+
+                // if the user guesses too big
+                } else if (userInput > 6) {
+                    System.out.println("Your integer is too large! This won't count.");
+
+                // if the user guesses too small
+                } else if (userInput < 1) {
+                    System.out.println("Your integer is too small! This won't count.");
+
+                // if the user is wrong
+                } else {
+                    System.out.println("You are wrong. Try again!");
+                    totalTries = totalTries + 1;
+                }
+
+            }
+
+        // block of code to handle errors
+        } catch (java.util.InputMismatchException errorCode) {
+
+            System.out.println("\nYou have not entered a valid input.");
+
+        }
+
+        // done
+        System.out.println("\nDone.");
+    }
+}
+
---
+++ package-info.java
@@ -0,0 +1,9 @@
+/**
+* This is the package-info.java file.
+*
+* @author  Myles Trump
+* @version 1.0
+* @since   2021-11-24
+*/
+package ca.mths.ics4u;
+  
